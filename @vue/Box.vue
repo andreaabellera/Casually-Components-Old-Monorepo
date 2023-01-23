113de713c715d59fd4880cbe10fd948f61f0d78e
Andrea Abellera <andrea.abellera@gmail.com>
2023-01-23 14:55:54 -0600

    Translate Box and Card components for Vue

---
+++ @vue/Box.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import "../assets/skin.css"
+    import "../assets/container.css"
+    import { ref } from "vue"
+
+    const props = defineProps
+    ({ 
+        skin: {type: String, required: false, default: "platinum"},
+        width: {type: String, required: false, default: "max-content"},
+        height: {type: String, required: false, default: "max-content"},
+        margin: {type: String, required: false, default: "1.5em"},
+        padding: {type: String, required: false, default: "1em"},
+        row: {type: Boolean, required: false, default: false}
+    })
+
+    const rowClass = ref("")
+    if(props.row) { rowClass.value = "boxRowLayout" }
+</script>
+
+<template>
+    <div id="box" :class="`${props.skin} ${rowClass}`" :style="`height:${props.height};width:${props.width};margin:${props.margin};padding:${props.padding};`" >
+        <slot />
+    </div>
+</template>
---
+++ @vue/Card.vue
@@ -0,0 +1,35 @@
+<script setup>
+    import "../assets/skin.css"
+    import "../assets/container.css"
+    import { ref } from "vue"
+
+    const props = defineProps
+    ({ 
+        skin: {type: String, required: false, default: "platinum"},
+        link: {type: String, required: false, default: null},
+        image: {type: String, required: false, default: null},
+        imagePosition: {type: String, required: false, default: "left"},
+        width: {type: String, required: false, default: "max-content"},
+        height: {type: String, required: false, default: "max-content"},
+        margin: {type: String, required: false, default: "1.5em"}
+    })
+
+    const posClass = ref("")
+    if(props.image && props.imagePosition=="top") { posClass.value = "imgOnTop" }
+    else if(props.image && props.imagePosition=="right") { posClass.value = "imgOnRight" }
+    else if(props.image && props.imagePosition=="bottom") { posClass.value = "imgOnBottom" }
+    else if(props.image && props.imagePosition=="left"){ posClass.value = "imgOnLeft" }
+</script>
+
+<template>
+    <a v-if="link" :href="link" id="card" :class="`${props.skin} ${posClass}`" :style="`height:${props.height};width:${props.width};margin:${props.margin};padding:${props.padding};`" >
+        <div v-if="image && (imagePosition=='left' || imagePosition=='top')" class="cardThumb" :style="`background-image:url(${image});`"></div>
+        <div class="cardInner"><slot /></div>
+        <div v-if="image && (imagePosition=='right' || imagePosition=='bottom')" class="cardThumb" :style="`background-image:url(${image});`"></div>
+    </a>
+    <div v-else id="card" :class="`${props.skin} ${posClass}`" :style="`height:${props.height};width:${props.width};margin:${props.margin};padding:${props.padding};`" >
+        <div v-if="image && (imagePosition=='left' || imagePosition=='top')" class="cardThumb" :style="`background-image:url(${image});`"></div>
+        <div class="cardInner"><slot /></div>
+        <div v-if="image && (imagePosition=='right' || imagePosition=='bottom')" class="cardThumb" :style="`background-image:url(${image});`"></div>
+    </div>
+</template>
