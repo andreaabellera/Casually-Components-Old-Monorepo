d7e72ed1d708998a4c014d37b051714806d09d05
Andrea Abellera <andrea.abellera@gmail.com>
2023-01-30 17:48:03 -0600

    Update test site for Angular integration
    Angular test site remade with Vite, significantly improved, and can integrate .ts components remotely as is ❤️

--- test/angular-site/src/app.module.ts
+++ test/angular-site/src/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-//import { CasuallyButton } from './button';
 import { CasuallyButton } from "casually-components/@angular/button";
 
 @NgModule({
