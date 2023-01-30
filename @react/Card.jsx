242bf89c454acfa7e7b7b337108951dd86ba03c1
Andrea Abellera <andrea.abellera@gmail.com>
2023-01-29 23:05:56 -0600

    Fix validateDOMNesting for React Card
    Ahah <a> creation works! Quite elegant

--- @react/Card.jsx
+++ @react/Card.jsx
@@ -14,37 +14,34 @@ export default function Card({
     }){
 
   const [posClass, setPosClass] = useState("")
+  const [hoverClass, setHoverClass] = useState("")
   useEffect(() => {
     if(image && imagePosition=="top") { setPosClass("imgOnTop") }
     else if(image && imagePosition=="right") { setPosClass("imgOnRight") }
     else if(image && imagePosition=="bottom") { setPosClass("imgOnBottom") }
     else if(image && imagePosition=="left"){ setPosClass("imgOnLeft") }
+    if(link) { setHoverClass("hoverable") }
   }, [])
 
+  function generateRedirect(){
+    if(link){
+        var a = document.createElement("a")
+        document.body.appendChild(a)
+        a.href = link
+        a.click()
+        window.URL.revokeObjectURL(link)
+    }
+  }
+
   return ( 
-    <>
-    {link && 
-        <a href={link} id="card" className={`${skin} ${posClass}`} style={{height: height, width: width, margin: margin}}>
-            {image && (imagePosition=="left" || imagePosition=="top") && 
-                <div className="cardThumb" style={{backgroundImage: `url(${image})`}} ></div>
-            }
-            <div className="cardInner">{children}</div>
-            {image && (imagePosition=="right" || imagePosition=="bottom") &&
-                <div className="cardThumb" style={{backgroundImage: `url(${image})`}}></div>
-            }
-        </a>
-    }
-    {!link && 
-        <div id="card" className={`${skin} ${posClass}`} style={{height: height, width: width, margin: margin}}>
-            {image && (imagePosition=="left" || imagePosition=="top") &&
-                <div className="cardThumb" style={{backgroundImage: `url(${image})`}}></div>
-            }
-            <div className="cardInner">{children}</div>
-            {image && (imagePosition=="right" || imagePosition=="bottom") &&
-                <div className="cardThumb" style={{backgroundImage: `url(${image})`}}></div>
-            }
-        </div>
-    }
-    </>
+    <div onClick={generateRedirect} id="card" className={`${skin} ${posClass} ${hoverClass}`} style={{height: height, width: width, margin: margin}} role="link" aria-label={`Clicking this card will redirect page to ${link}`}>
+        {image && (imagePosition=="left" || imagePosition=="top") &&
+            <div className="cardThumb" style={{backgroundImage: `url(${image})`}}></div>
+        }
+        <div className="cardInner">{children}</div>
+        {image && (imagePosition=="right" || imagePosition=="bottom") &&
+            <div className="cardThumb" style={{backgroundImage: `url(${image})`}}></div>
+        }
+    </div>
   )
 }
